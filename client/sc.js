1082066257a95f8bc987b14060fbc04b5248c3ad
Nick McMurdie <nickmcmurdie@gmail.com>
2023-02-02 14:29:09 -0700

    Comment code for readability

--- client/sc.js
+++ client/sc.js
@@ -5,16 +5,19 @@ const GRID_HEIGHT = 9;
 const GRID_COLOR_PRIMARY = "#dbb8a1";
 const GRID_COLOR_SECONDARY = "#7d5942";
 
+// Init canvas and start game loop
 document.addEventListener("DOMContentLoaded", () => {
     let ctx = initCanvas();
 });
 
+// Draw / scale the initial game board and return the canvas context
 function initCanvas() {
     const canv = document.getElementById("game_canvas");
     const ctx = canv.getContext("2d");
 
+    // Make sure the width of the canvas is the closest multiple of the grid height
     let containerHeight = document.getElementById("game_container").clientHeight;
-    containerHeight = containerHeight - (containerHeight % GRID_WIDTH);
+    containerHeight = containerHeight - (containerHeight % GRID_HEIGHT);
     canv.width = containerHeight;
     canv.height = containerHeight;
 
