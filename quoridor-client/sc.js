72e64c6d074ae8a71624d2049017b1e577ab0011
Jake Edvenson <jakers.edvenson@gmail.com>
2023-02-09 08:43:35 -0700

    Include initial walls

--- quoridor-client/sc.js
+++ quoridor-client/sc.js
@@ -4,7 +4,8 @@ const GRID_WIDTH = 9;
 const GRID_HEIGHT = 9;
 const GRID_COLOR_PRIMARY = "#dbb8a1";
 const GRID_COLOR_SECONDARY = "#7d5942";
-const WALL_ACTIVE_COLOR = "#38281e";
+const WALL_INACTIVE_COLOR = "#38281e";
+const WALL_ACTIVE_COLOR = "#f66206";
 
 // Init canvas and start game loop
 document.addEventListener("DOMContentLoaded", () => {
@@ -19,36 +20,58 @@ function initCanvas() {
     // Make sure the width of the canvas is the closest multiple of the grid height
     let containerHeight = document.getElementById("game_container").clientHeight;
     containerHeight = containerHeight - (containerHeight % GRID_HEIGHT);
-    canv.width = containerHeight;
-    canv.height = containerHeight;
+    canv.width = containerHeight + (containerHeight / 8);
+    canv.height = containerHeight + (containerHeight / 8);
 
     const squareSize = Math.floor(containerHeight / GRID_WIDTH);
+    const offset = Math.floor(squareSize/8);
     console.log("Square size", squareSize);
 
     for (let i = 0; i < GRID_WIDTH; i++) {
         for (let j = 0; j < GRID_HEIGHT; j++) {
             ctx.fillStyle = ((i + j) % 2) ? GRID_COLOR_PRIMARY : GRID_COLOR_SECONDARY;
-            ctx.fillRect(i * squareSize, j * squareSize, squareSize, squareSize);
+            ctx.fillRect((i * squareSize) + (i * offset), (j * squareSize) + (j * offset), squareSize, squareSize);
+            ctx.fillStyle = WALL_INACTIVE_COLOR;
+            //Handle filling in the inactive vertical walls
+            if(i != 0){
+                //Fills in vertical inactive walls & covers the space between verticle & horizontal walls
+                if(j != GRID_WIDTH-1){
+                    ctx.fillRect((i * squareSize) + ((i-1) * offset), (j * squareSize) + (j * offset), offset, squareSize+offset);
+                }
+                else{
+                    ctx.fillRect((i * squareSize) + ((i-1) * offset), (j * squareSize) + (j * offset), offset, squareSize);
+                }
+            }
+            //Handle filling in the inactive horizontal walls
+            if(j != 0){
+                ctx.fillRect((i * squareSize) + (i * offset), (j * squareSize) + ((j-1) * offset), squareSize, offset);
+            }
         }
     }
+    addHorizontalWall(ctx, squareSize, 7, 8);
     return ctx;
 }
 
 //Draws vertical walls based upon row/column location
 function addVerticalWall(ctx, squareSize, column, row) {
+    const offset = Math.floor(squareSize/8); //The offset used for walls
+
     //X is valid at 1-8, Y is valid at 0-7
     let x = column * squareSize; // Calculates x/y location based upon array location & squareSize
     let y = row * squareSize;
 
     ctx.fillStyle = WALL_ACTIVE_COLOR; 
-    ctx.fillRect(x-squareSize/16, y, squareSize/8, squareSize * 2);
+    ctx.fillRect(x + ((column - 1) * offset), y + (row * offset), offset, (squareSize * 2) + offset)
 }
 
 //Draws horizontal walls based upon row/column location
 function addHorizontalWall(ctx, squareSize, column, row) {
+    const offset = Math.floor(squareSize/8); //The offset used for walls
+
+    //X is valid at 0-7, Y is valid at 1-8
     let x = column * squareSize;
     let y = row * squareSize;
 
     ctx.fillStyle = WALL_ACTIVE_COLOR;
-    ctx.fillRect(x, y-squareSize/16, squareSize*2, squareSize/8);
+    ctx.fillRect(x + (column * offset), y + ((row-1) * offset), (squareSize * 2) + offset, offset);
 }
