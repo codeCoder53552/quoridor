c5a46b124f0532e303031a06869c92703863174e
Tanner Ellison <tegolf97@gmail.com>
2023-04-05 22:44:06 -0600

    Changed the name of the players to correspond with the ones used in the board class.

--- quoridor-server/game.py
+++ quoridor-server/game.py
@@ -3,28 +3,34 @@ from board import Board
 class QuoridorGame:
     def __init__(self):
         self.board = Board()
-        self.players = {1: None, 2:None}
+        self.players = {"player_n": None, "player_s":None, "player_e":None, "player_w":None}
         self.gameOver = False
 
     def add_player(self, playerID):
         players = self.players
+        # This index will just be there to track how many players we want. For now it's just two.
+        index = 0
         for playerNum in players:
-            if players.get(playerNum) == playerID:
+            if index == 1:
+                return False
+            elif players.get(playerNum) == playerID:
                 return True
             elif not players.get(playerNum):
                 players[playerNum] = playerID
                 return True
+            index+=1
+
         return False
     
     def remove_player(self, playerID):
         playerIDs = list(self.players.values())
-        playerNums = list(self.players.keys())
+        playerNames = list(self.players.keys())
         try:
             position = playerIDs.index(playerID)
         except ValueError:
             return None
             
-        playerNum = playerNums[position]
+        playerNum = playerNames[position]
 
         try:
             self.players[playerNum] = None
