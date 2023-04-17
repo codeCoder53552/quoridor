f3c88d3d53a34320eba711f2209d77156b71f4f5
Colby Weber <cweber@wiktel.com>
2023-04-17 12:08:23 -0600

    Should've fixed joining ended games bug

--- quoridor-server/server.py
+++ quoridor-server/server.py
@@ -60,7 +60,7 @@ def make_room(roomType: str, isOpen: bool = False):
             for room in openRooms[roomType]:
                 # positive numbers mean spots left
                 # negative numbers mean infinite spots left
-                if room.spotsLeft != 0:
+                if room.spotsLeft != 0 and (room._type != "game" or room.game.gameOver):
                     return room.id
 
     ## make a normal room
